5f32bc77a72b400f80e8ec40a90a93bd161c821e
Mike Keith <michael.keith@oracle.com>
2011-10-11 21:28:41 +0000

    made a copy
    git-svn-id: file:///svnroot/rt/org.eclipse.gemini.jpa/branches/1.0.0@192 738bc060-e27f-0410-be59-ab60bd4d2b7a

---
+++ org.eclipse.gemini.jpa.test.weavedpunit/src/model/weaved/WeavedEntity.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Oracle.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution. 
+ * The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at 
+ *     http://www.opensource.org/licenses/apache2.0.php.
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *     mkeith - Gemini JPA tests 
+ ******************************************************************************/
+package model.weaved;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+
+/**
+ * Test JPA model class
+ * 
+ * @author mkeith
+ */
+@Entity
+public class WeavedEntity {
+
+    @Id
+    int id;
+    
+    // Lazy fetching of a basic attribute will cause the entity to be woven
+    @Basic(fetch=FetchType.LAZY) 
+    String lazyString;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getLazyString() { return lazyString; }
+    public void setLazyString(String lazyString) { this.lazyString = lazyString; }
+}
