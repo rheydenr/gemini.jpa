12786160661871435ebdc8e384303efb6a22dee2
mkeith <michael.keith@oracle.com>
2012-05-22 12:00:05 -0400

    Added GeminiPersistenceUnitProperties.java file (closure for bug 375628)

---
+++ org.eclipse.gemini.jpa/src/org/eclipse/gemini/jpa/GeminiPersistenceUnitProperties.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Oracle.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution. 
+ * The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at 
+ *     http://www.opensource.org/licenses/apache2.0.php.
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *     mkeith - Gemini JPA work 
+ ******************************************************************************/
+package org.eclipse.gemini.jpa;
+
+/**
+ * Gemini-specific persistence unit properties that can be configured 
+ * in the persistence descriptor or passed into an EMF creation method.
+ */
+public class GeminiPersistenceUnitProperties {
+    
+    /*==================*/
+    /* Static constants */
+    /*==================*/
+       
+}
